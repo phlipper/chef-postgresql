d017d7bd524362787c557fb20321787440550db0
Phil Cohen <github@phlippers.net>
2012-06-29 10:14:10 -0700

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "github@phlippers.net"
 license          "MIT"
 description      "Installs PostgreSQL, The world's most advanced open source database."
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "0.1.0"
+version          "0.2.0"
 
 recipe "postgresql",           "Set up the apt repository and install dependent packages"
 recipe "postgresql::client",   "Front-end programs for PostgreSQL 9.x"
