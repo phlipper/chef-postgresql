463f8de60ccde6bfaa18a9f720e2423aa3ee1c03
Alexey Noskov <alexey.noskov@gmail.com>
2012-09-29 22:23:25 +0400

    pg_database_extensions definition

---
+++ definitions/pg_database_extensions.rb
@@ -0,0 +1,48 @@
+define :pg_database_extensions, :action => :create do
+
+  dbname = params[:name]
+  languages = params[:languages] || []
+  extensions = params[:extensions] || []
+  postgis = params[:postgis]
+
+  postgresql_version = node["postgresql"]["version"]
+  postgis_version = node["postgis"]["version"]
+
+  case params[:action]
+  when :create
+
+    languages.each do |language|
+      execute "createlang #{language} #{dbname}" do
+        user "postgres"
+        not_if "psql -c 'SELECT lanname FROM pg_catalog.pg_language' #{dbname} | grep '^ #{language}$'", :user => "postgres"
+      end
+    end
+
+    extensions.uniq.each do |extension|
+      execute "psql -c 'CREATE EXTENSION IF NOT EXISTS #{extension}' #{dbname}" do
+        user "postgres"
+      end
+    end
+
+    if postgis
+      include_recipe 'postgresql::postgis'
+
+      execute "psql -d #{dbname} -f /usr/share/postgresql/#{postgresql_version}/contrib/postgis-#{postgis_version}/postgis.sql" do
+        user "postgres"
+        not_if "psql -c \"SELECT proname FROM pg_catalog.pg_proc WHERE proname = 'st_area'\" #{dbname} | grep 'st_area$'", :user => "postgres"
+      end
+
+      execute "psql -d #{dbname} -f /usr/share/postgresql/#{postgresql_version}/contrib/postgis-#{postgis_version}/spatial_ref_sys.sql" do
+        user "postgres"
+        only_if "psql -c 'SELECT count(1) FROM spatial_ref_sys' #{dbname} | grep '0$'", :user => "postgres"
+      end
+
+      [:geometry_columns, :geography_columns, :spatial_ref_sys].each do |table|
+        execute "psql -c 'GRANT ALL ON #{table} TO PUBLIC' #{dbname}" do
+          user "postgres"
+        end
+      end
+    end
+
+  end
+end
