5a6c7281c2fa105eedfa65d44853e8c2147b9112
Phil Cohen <github@phlippers.net>
2013-07-23 10:12:36 -0700

    setup `server_dev` like the `server` recipe

--- recipes/server_dev.rb
+++ recipes/server_dev.rb
@@ -12,3 +12,18 @@ end
 
 # install the package
 package "postgresql-server-dev-#{node["postgresql"]["version"]}"
+
+# setup the data directory
+include_recipe "postgresql::data_directory"
+
+# add the configuration
+include_recipe "postgresql::configuration"
+
+# setup users
+include_recipe "postgresql::pg_user"
+
+# setup databases
+include_recipe "postgresql::pg_database"
+
+# declare the system service
+include_recipe "postgresql::service"
