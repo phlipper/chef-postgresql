9a04df2f8e23a3c342bcaa598c16ed37d8325827
Phil Cohen <github@phlippers.net>
2013-09-13 12:45:45 -0700

    cleanup formatting

--- recipes/debian_backports.rb
+++ recipes/debian_backports.rb
@@ -5,13 +5,11 @@
 
 
 # backports for initial support
-backports_uri = case node["lsb"]["codename"]
-                  when 'wheezy'
-                    "http://cdn.debian.net/debian"
-                  else
-                    "http://backports.debian.org/debian-backports"
-                end
-
+backports_uri = if node["lsb"]["codename"] == "wheezy"
+  "http://cdn.debian.net/debian"
+else
+  "http://backports.debian.org/debian-backports"
+end
 
 apt_repository "debian-backports" do
   uri backports_uri
