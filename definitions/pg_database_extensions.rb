8b128aaf4a9729e794d5b417cc130adb1cd6f3c1
Phil Cohen <github@phlippers.net>
2013-09-15 14:32:45 -0700

    update extensions definition for latest postgis

--- definitions/pg_database_extensions.rb
+++ definitions/pg_database_extensions.rb
@@ -1,12 +1,9 @@
 define :pg_database_extensions, action: :create do
 
   dbname = params[:name]
-  languages = [params[:languages] || []].flatten # Allow single value or array of values
-  extensions = [params[:extensions] || []].flatten
+  languages = Array(params[:languages]) # Allow single value or array of values
+  extensions = Array(params[:extensions])
   postgis = params[:postgis]
-
-  postgresql_version = node["postgresql"]["version"]
-  postgis_version = node["postgis"]["version"]
 
   case params[:action]
   when :create
@@ -27,18 +24,8 @@ define :pg_database_extensions, action: :create do
     if postgis
       include_recipe "postgresql::postgis"
 
-      execute "psql -d #{dbname} -f /usr/share/postgresql/#{postgresql_version}/contrib/postgis-#{postgis_version}/postgis.sql" do
-        user "postgres"
-        not_if %(psql -c "SELECT proname FROM pg_catalog.pg_proc WHERE proname = 'st_area'" #{dbname} | grep 'st_area$'), user: "postgres"
-      end
-
-      execute "psql -d #{dbname} -f /usr/share/postgresql/#{postgresql_version}/contrib/postgis-#{postgis_version}/spatial_ref_sys.sql" do
-        user "postgres"
-        only_if "psql -c 'SELECT count(1) FROM spatial_ref_sys' #{dbname} | grep '0$'", user: "postgres"
-      end
-
-      [:geometry_columns, :geography_columns, :spatial_ref_sys].each do |table|
-        execute "psql -c 'GRANT ALL ON #{table} TO PUBLIC' #{dbname}" do
+      %w[postgis postgis_topology].each do |ext|
+        execute "psql -c 'CREATE EXTENSION IF NOT EXISTS #{ext}' #{dbname}" do
           user "postgres"
         end
       end
@@ -60,7 +47,11 @@ define :pg_database_extensions, action: :create do
     end
 
     if postgis
-      Chef::Log.warn("Postgis support dropping isn't supported")
+      %w[postgis postgis_topology].each do |ext|
+        execute "psql -c 'DROP EXTENSION IF EXISTS #{ext}' #{dbname}" do
+          user "postgres"
+        end
+      end
     end
 
   end
