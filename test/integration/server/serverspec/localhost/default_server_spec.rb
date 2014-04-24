7285da097c4e5c7f7380b17307c5ac588f1f0740
Phil Cohen <github@phlippers.net>
2014-04-24 02:29:25 -0300

    use past tense for integration tests

--- test/integration/server/serverspec/localhost/default_server_spec.rb
+++ test/integration/server/serverspec/localhost/default_server_spec.rb
@@ -4,13 +4,13 @@ describe "PostgreSQL `apt` setup" do
   let(:apt_src){ "/etc/apt/sources.list.d/apt.postgresql.org.list" }
   let(:apt_pref){ "/etc/apt/preferences.d/pgdg.pref" }
 
-  it "sets up default apt preferences" do
+  it "set up default apt preferences" do
     expect(file apt_pref).to be_file
     expect(file apt_pref).to contain("Package: *")
     expect(file apt_pref).to contain("Pin: release o=apt.postgresql.org")
   end
 
-  it "sets up the primay apt repository" do
+  it "set up the primay apt repository" do
     expect(file apt_src).to be_file
     expect(file apt_src).to contain("apt.postgresql.org")
     expect(file apt_src).to contain("pgdg main")
@@ -35,43 +35,40 @@ describe "PostgreSQL server installation" do
     "local   all             postgres                                peer"
   end
 
-  it "manages the `postgresql` service" do
+  it "managed the `postgresql` service" do
     expect(service "postgresql").to be_enabled
-    # expect(service "postgresql").to be_running
+    expect(service "postgresql").to be_running
     expect(command "pgrep postgres").to return_stdout(/\d+/)
   end
 
-  it "listens on the default port" do
+  it "listened on the default port" do
     expect(port 5432).to be_listening
   end
 
-  it "configures a default `postgresql.conf`" do
+  it "configured a default `postgresql.conf`" do
     expect(file "#{config_path}/postgresql.conf").to be_a_file
     expect(file "#{config_path}/postgresql.conf").to contain(
-      %|hba_file = '/etc/postgresql/9.3/main/pg_hba.conf'|
+      %(hba_file = '/etc/postgresql/9.3/main/pg_hba.conf')
     )
   end
 
-  it "configures a default `pg_hba.conf`" do
+  it "configured a default `pg_hba.conf`" do
     expect(file "#{config_path}/pg_hba.conf").to be_a_file
-    # expect(file "#{config_path}/pg_hba.conf").to contain(
-    expect(command "sudo grep '#{hba_contents}' #{config_path}/pg_hba.conf").to(
-      return_stdout(hba_contents)
-    )
+    expect(file "#{config_path}/pg_hba.conf").to contain(hba_contents)
   end
 end
 
 describe "PostgreSQL users, databases, and extensions" do
-  it "creates the `testuser` user" do
+  it "created the `testuser` user" do
     database_role_exists? "testuser"
   end
 
-  it "creates the `testdb` database" do
+  it "created the `testdb` database" do
     database_exists? "testdb"
   end
 
   %w[dblink hstore uuid-ossp].each do |extension|
-    it "creates the `#{extension}` database extension" do
+    it "created the `#{extension}` database extension" do
       database_extension_exists? "testdb", extension
     end
   end
