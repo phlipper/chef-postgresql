52346efd529a93bd5f11ee7700d9d358e647a822
Phil Cohen <github@phlippers.net>
2014-08-24 19:25:40 -0700

    strict about rspec deprecations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,7 @@ RSpec.configure do |config|
   config.platform = "ubuntu"
   config.version = "12.04"
   config.log_level = :error
+  config.raise_errors_for_deprecations!
 end
 
 def add_apt_repository(resource_name)
